5b329b6e8eecafec4b5b45a794deec19f3556b4e
Shingo W. Kagami <swk@kagami.org>
2018-09-28 19:42:33 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Raspberry Pi Endless Video Player
+
+## Installation for 2018-06-27-raspbian-stretch-lite in Raspberry Pi Zero W
+
+- Uncomment `hdmi_force_hotplug=1` in `/boot/config.txt`
+- Add ` consoleblank=600` (or any of your favorite duration in second) at the end of `/boot/cmdline.txt`
+- Choose `console autologin` by running `raspi-config` (Boot Option - Desktop / CLI)
+- Install `omxplayer` and `python3-rpi.gpio` by `apt-get`
+- Put `videoplayer.py` (the file distributed here) at `~pi/script/`
+- Put video files in `~pi/Videos/`  (which are played in the dictionary order of the file names)
+- Add the following line in crontab of user `pi` by running `crontab -e`:
+```
+@reboot	/home/pi/script/videoplayer.py > /dev/null 2>&1 &
+```
+
+## Operation
+
+- Connect HDMI output to a display and power input (or USB input) to 5V power source; The videos are automatically started.
+
+- Connect BCM 23 (Pin 16 in Pi Zero) to Gnd (e.g. Pin 14) to stop playing; Let the pin open to start again from the next video
+- Connect BCM 24 (Pin 18 in Pi Zero) to Gnd to shut down the OS
---
+++ videoplayer.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+
+import sys
+import subprocess
+import RPi.GPIO as GPIO
+import time
+import os
+import glob
+
+# vids = [ '/home/pi/Videos/kagami_siggraph2018.mp4',
+#          '/home/pi/Videos/SIGGRAPH2018_demo_1280x720.mp4' ]
+vids = glob.glob('/home/pi/Videos/*.*')
+
+pin_shutdown = 24
+pin_stop = 23
+
+
+def doAndRetry(fn, n_retries, interval_sec):
+    for i in range(n_retries):
+        try:
+            fn()
+        except Exception as e:
+            print("exception: {e}; retry {i}/{max}"
+                  .format(e = e, i= i + 1, max = n_retries))
+            time.sleep(i * interval_sec)
+        else:
+            break
+
+class VideoPlayer:
+    def __init__(self):
+        self.proc = None
+        self.state_stop = False
+
+    def stopVideo(self):
+        os.system("TERM=linux sudo setterm -blank poke < /dev/tty1")
+        if self.proc is not None and self.proc.poll() is None:
+            self.proc.stdin.write('q')
+            self.proc.stdin.flush()
+            print("q sent")
+        self.state_stop = True
+    
+    def callBackShutdown(self, channel):
+        time.sleep(1)
+        if GPIO.input(pin_shutdown) == 0:
+            self.stopVideo()
+            print("shutting down")
+            os.system("sudo echo shutting down... > /dev/tty1")
+            time.sleep(1)
+            os.system("sudo shutdown -h now")
+    
+    def callBackStop(self, channel):
+        time.sleep(1)
+        if GPIO.input(pin_stop) == 0:
+            self.stopVideo()
+            print("stop")
+        else:
+            self.state_stop = False
+            print("play")
+    
+    def mainLoop(self):
+        n_vids = len(vids)
+        k = 0
+        while True:
+            if self.state_stop == True:
+                time.sleep(1)
+                continue
+
+            def blankTerminal():
+                subprocess.check_call(
+                    "TERM=linux sudo setterm -blank force < /dev/tty1", 
+                    shell=True
+                )
+            doAndRetry(blankTerminal, n_retries = 5, interval_sec = 10)
+    
+            cmd = "omxplayer -o local %s" % vids[k]
+            print(cmd)
+            self.proc = subprocess.Popen(cmd, shell=True, 
+                                         stdin=subprocess.PIPE, 
+                                         universal_newlines=True)
+            self.proc.wait()
+            k = (k + 1) % n_vids
+
+if __name__ == "__main__":
+    sys.stdout = os.fdopen(sys.stdout.fileno(), "w", buffering=1)
+    vp = VideoPlayer();
+
+    print("initializing GPIO...")
+    def initGPIO():
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pin_stop, GPIO.IN, GPIO.PUD_UP)
+        GPIO.setup(pin_shutdown, GPIO.IN, GPIO.PUD_UP)
+        GPIO.add_event_detect(pin_stop, GPIO.BOTH, 
+                              callback=vp.callBackStop, bouncetime=300) 
+        GPIO.add_event_detect(pin_shutdown, GPIO.FALLING, 
+                              callback=vp.callBackShutdown, bouncetime=300)
+    doAndRetry(initGPIO, n_retries = 5, interval_sec = 5)
+
+    try:
+        print("main")
+        vp.mainLoop()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        print("KeyboardInterrupt")
